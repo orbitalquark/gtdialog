a4d8e4093e383509d8fdf2d9ba7092b162ca43b4
mitchell <70453897+orbitalquark@users.noreply.github.com>
2009-07-30 17:14:01 -0400

    Fixed GTK_STOCK_OPEN -> GTK_STOCK_SAVE typo; gcocoadialog.c

--- gcocoadialog.c
+++ gcocoadialog.c
@@ -143,7 +143,7 @@ char *gcocoadialog(GCDialogType type, int narg, const char *args[]) {
   } else if (type == GCDIALOG_FILESAVE) {
     dialog = gtk_file_chooser_dialog_new("", NULL, GTK_FILE_CHOOSER_ACTION_SAVE,
                                          GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-                                         GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+                                         GTK_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
                                          NULL);
     gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(dialog),
                                                    TRUE);
