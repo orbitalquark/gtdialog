7e020ff1e560cc42768e0812c20c5c3cafdbcd21
mitchell <70453897+orbitalquark@users.noreply.github.com>
2015-12-31 16:59:53 -0500

    Updated copyright date.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# Copyright 2009-2015 Mitchell mitchell.att.foicica.com.
+# Copyright 2009-2016 Mitchell mitchell.att.foicica.com.
 
 CC = gcc
 PREFIX ?= /usr/local
--- gtdialog.c
+++ gtdialog.c
@@ -3,7 +3,7 @@
  *
  * The MIT License
  *
- * Copyright (c) 2009-2015 Mitchell mitchell.att.foicica.com
+ * Copyright (c) 2009-2016 Mitchell mitchell.att.foicica.com
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
