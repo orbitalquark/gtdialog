7be306bccabd8c16d6d5e7e0a48bfb0a6453e536
mitchell <70453897+orbitalquark@users.noreply.github.com>
2016-08-23 16:50:54 -0400

    Reversed button order to match human interface guidelines (HIG); gtdialog.c For example, "Ok" should be on the right, and "Cancel" on the left.

--- gtdialog.c
+++ gtdialog.c
@@ -747,10 +747,13 @@ char *gtdialog(GTDialogType type, int narg, const char *args[]) {
 #elif CURSES
       int nbuttons = 0;
       for (i = 0; i < 3; i++) if (buttons[i]) nbuttons++;
+      char *rtl_buttons[3] = {NULL, NULL, NULL};
+      for (i = nbuttons; i > 0; i--)
+        rtl_buttons[nbuttons - i] = (char *)buttons[i - 1];
       buttonbox = newCDKButtonbox(dialog, 0, BOTTOM, 1, 0, "", 1, nbuttons,
-                                  (char **)buttons, nbuttons, A_REVERSE, TRUE,
+                                  rtl_buttons, nbuttons, A_REVERSE, TRUE,
                                   FALSE);
-      setCDKButtonboxCurrentButton(buttonbox, 0);
+      setCDKButtonboxCurrentButton(buttonbox, nbuttons - 1);
 #endif
     }
     // Create dialog content.
@@ -1154,6 +1157,8 @@ char *gtdialog(GTDialogType type, int narg, const char *args[]) {
       // activateCDKButtonbox returns -1 on escape so check for response == 0.
       if (response == 0) response = RESPONSE_DELETE;
     }
+    if (response != RESPONSE_DELETE)
+      response = buttonbox->buttonCount - (response - 1); // buttons are RTL
     wborder(border, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '), wrefresh(border);
     delwin(border);
     destroyCDKButtonbox(buttonbox);
